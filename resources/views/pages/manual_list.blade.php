e9417cf50616b191372c2f93be68d9e463f013c4
Meand05 <djay.verlaar@icloud.com>
2025-09-12 14:07:18 +0200

    Ticket 5 voltooid

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -5,25 +5,31 @@
     </x-slot:head>
 
     <x-slot:breadcrumb>
-        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
+        <li>
+            <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/"
+                title="Manuals for '{{ $brand->name }}'">
+                {{ $brand->name }}
+            </a>
+        </li>
     </x-slot:breadcrumb>
-
 
     <h1>{{ $brand->name }}</h1>
 
-    <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
-
-
+    <p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
+    <div class="manual-container">
         @foreach ($manuals as $manual)
-
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
-
-            <br />
+            <div class="manual-item">
+                @if ($manual->locally_available)
+                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/"
+                        title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                    </a>
+                @else
+                    <a href="{{ $manual->url }}" target="_blank" title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                    </a>
+                @endif
+            </div>
         @endforeach
-
+    </div>
 </x-layouts.app>
